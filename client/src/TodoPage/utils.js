7ecddbe596e745432078ce9d2d94e2893f36badf
saivarshith2000 <hosvarshith@gmail.com>
2021-07-26 18:08:28 +0530

    Fixed datetime error in TodoItem, removed junk files

--- client/src/TodoPage/utils.js
+++ client/src/TodoPage/utils.js
@@ -1,16 +1,23 @@
+const MS_IN_A_DAY = 24 * 60 * 60 * 1000;
+
 export default function getTimeDiff(added, completed) {
     let t1 = new Date(added).getTime(),
         t2 = new Date(completed).getTime(),
         milliseconds = 0,
-        time = '';
-    if (isNaN(t1) || isNaN(t2)) return '';
+        time = "";
+    if (isNaN(t1) || isNaN(t2)) return "";
     if (t1 < t2) milliseconds = t2 - t1;
     else milliseconds = t1 - t2;
-    var days = Math.floor(milliseconds / 1000 / 60 / (60 * 24));
-    var date_diff = new Date(milliseconds);
-    if (days > 0) time += days + 'd ';
-    if (date_diff.getHours() > 0) time += date_diff.getHours() + 'h ';
-    if (date_diff.getMinutes() > 0) time += date_diff.getMinutes() + 'm ';
-    if (date_diff.getSeconds() > 0) time += date_diff.getSeconds() + 's ';
+    var days = Math.floor(milliseconds / MS_IN_A_DAY);
+    if (days > 0) {
+        time += days + "d ";
+        milliseconds -= days * MS_IN_A_DAY
+    }
+    var seconds = Math.floor((milliseconds / 1000) % 60);
+    var minutes = Math.floor((milliseconds / (1000 * 60)) % 60);
+    var hours = Math.floor((milliseconds / (1000 * 60 * 60)) % 24);
+    if (hours > 0) time += hours + "h ";
+    if (minutes > 0) time += minutes + "m ";
+    if (seconds > 0) time += seconds + "s ";
     return time;
 }
